064d34b9385950b53648abe2390bffcb6ea3b6e6
bittermeatball <nguyenhuunguyeny.ny@gmail.com>
2021-07-25 13:09:01 +0700

    Init project

---
+++ members.js
@@ -0,0 +1,167 @@
+const memberList = [
+  {
+    "fullName": "Pham Thi Nha Vy",
+    "department": "Bussiness",
+    "skill": "Head of Business"
+  },
+  {
+    "fullName": "Tran Thi Khanh Vy",
+    "department": "Bussiness",
+    "skill": "Head of Business"
+  },
+  {
+    "fullName": "Tran Dinh Phuc Nguyen",
+    "department": "Event",
+    "skill": "Events manager"
+  },
+  {
+    "fullName": "Pham Tran Thanh Lam",
+    "department": "Event",
+    "skill": "Event organizer"
+  },
+  {
+    "fullName": "Ho Nhat Thao Mien",
+    "department": "Event",
+    "skill": "Event organizer"
+  },
+  {
+    "fullName": "Nguyen Thi Thuy Linh",
+    "department": "Event",
+    "skill": "Event organizer"
+  },
+  {
+    "fullName": "Huynh Thi Ngoc Quyen",
+    "department": "Marketing",
+    "skill": "Head of Marketing"
+  },
+  {
+    "fullName": "Nguyen Thi Thao Hang",
+    "department": "Marketing",
+    "skill": "Social media"
+  },
+  {
+    "fullName": "Thai Khanh Huyen",
+    "department": "Marketing",
+    "skill": "Social media"
+  },
+  {
+    "fullName": "Van Le Thao Nguyen",
+    "department": "Marketing",
+    "skill": "Social media"
+  },
+  {
+    "fullName": "Le Hong Hai",
+    "department": "Marketing",
+    "skill": "Designer"
+  },
+  {
+    "fullName": "Nguyen Dinh Khanh Linh",
+    "department": "Marketing",
+    "skill": "Designer"
+  },
+  {
+    "fullName": "Do Thi Kim Oanh",
+    "department": "Marketing",
+    "skill": "Designer"
+  },
+  {
+    "fullName": "Tran Thi Thuy Duyen",
+    "department": "Marketing",
+    "skill": "Social media"
+  },
+  {
+    "fullName": "Phan Thi Thuy Linh",
+    "department": "Marketing",
+    "skill": "Social media"
+  },
+  {
+    "fullName": "Nguyen Linh Huong",
+    "department": "Marketing",
+    "skill": "Social media"
+  },
+  {
+    "fullName": "Nguyen Minh Duc",
+    "department": "Tech",
+    "skill": "Mobile developer"
+  },
+  {
+    "fullName": "Le Viet Trung",
+    "department": "Tech",
+    "skill": "Machine learning"
+  },
+  {
+    "fullName": "Pham Manh Dung",
+    "department": "Tech",
+    "skill": "Machine learning"
+  },
+  {
+    "fullName": "Nguyen Ngoc Quang",
+    "department": "Tech",
+    "skill": "Technical leader"
+  },
+  {
+    "fullName": "Nguyen Ba Hoang",
+    "department": "Tech",
+    "skill": "Machine learning"
+  },
+  {
+    "fullName": "Dang Ngoc Phu",
+    "department": "Tech",
+    "skill": "Technical leader"
+  },
+  {
+    "fullName": "Nguyen Tien Van",
+    "department": "Tech",
+    "skill": "Backend developer"
+  },
+  {
+    "fullName": "Do Huu Nguyen Thang",
+    "department": "Tech",
+    "skill": "Backend developer"
+  },
+  {
+    "fullName": "Le Lien Huong",
+    "department": "Tech",
+    "skill": "Frontend developer"
+  },
+  {
+    "fullName": "Nguyen Huu Duc Huy",
+    "department": "Tech",
+    "skill": "Frontend developer"
+  },
+  {
+    "fullName": "Nguyen Tan Phat",
+    "department": "Tech",
+    "skill": "Frontend developer"
+  },
+  {
+    "fullName": "Phan Thanh Huy",
+    "department": "Tech",
+    "skill": "Technical leader"
+  },
+  {
+    "fullName": "Nguyen Minh Hieu",
+    "department": "Tech",
+    "skill": "Mobile developer"
+  },
+  {
+    "fullName": "Le Vinh Nhan Duc",
+    "department": "Tech",
+    "skill": "Mobile developer"
+  },
+  {
+    "fullName": "Le Thi Kim Chi",
+    "department": "Tech",
+    "skill": "Backend developer"
+  },
+  {
+    "fullName": "Nguyen Ich Hoa",
+    "department": "Tech",
+    "skill": "Backend developer"
+  },
+  {
+    "fullName": "Huynh Thi Khanh Linh",
+    "department": "Tech",
+    "skill": "Frontend developer"
+  }
+]
